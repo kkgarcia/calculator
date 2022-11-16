e68a47e98a69b2519e60cbcefa8e897ad6b353de
kkgarcia <rus135.ra@gmail.com>
2022-11-16 17:48:31 +0300

    font correction

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const buttons = document.querySelectorAll('.button-container button');
-const numbers = document.querySelectorAll('.number');
 const displayContent = document.querySelector('.display-content');
-const funcButtons = document.querySelectorAll('.func');
-const point = document.querySelector('.point')
 
 function add(a, b) {
     return Math.round((+a + +b)*100000)/100000;
@@ -56,31 +53,21 @@ buttons.forEach(button => {
         if (button.classList[0] == 'number' && first.length < 14) {
             first += button.innerText
             displayValue(first)
-            console.log(first)
         } else if (button.classList[1] == 'lul') {
             if (list.length == 0 && !result) {
                 list.push(first, button.innerText)
-                console.log('here')
                 clearText()
                 displayValue(first)
-                console.log(list)
             } else if(result && list.length == 0) {
                 list.push(result, button.innerText)
-                console.log("midddleee")
-                console.log(list)
             } else {
                 if (first) {
                     result = operate(list.pop(), list.pop(), first)
                     list.push(result, button.innerText)
                     displayValue(result)
                     clearText()
-                    console.log('doown')
-                    console.log(list)
-
                 } else {
                     list[1] = button.innerText
-                    console.log('broooo')
-                    console.log(list)
                 }
             }
         } else if (event.target.innerText == 'AC') {
@@ -91,7 +78,7 @@ buttons.forEach(button => {
                 result = operate(list.pop(), list.pop(), first)
                 displayValue(result)
                 clearText()
-            } else console.log('returrrn')
+            } else return
         } else if (event.target.innerText == '0' && first.length < 14) {
             if (displayContent.innerText) {
                 first += '0';
@@ -102,54 +89,41 @@ buttons.forEach(button => {
                 first = '0.'
                 displayValue(first)
             } else if (first.includes('.')) {
-                console.log('return')
                 return
             } else {
                 if (result) {
                     first += '0.'
                     displayValue(first)
-                    console.log("incddddd")
                 } else {
                     first += '.'
                     displayValue(first)
-                    console.log("adfaaaaaaa")
                 }
             }
         } else if (event.target.innerText == '+/-') {
             if (Math.sign(first) == -1) {
                 first = Math.abs(first)
                 displayValue(first)
-                console.log('mere')
-                console.log(first)
             } else if (Math.sign(result) == -1 && !first) {
                 if (list.length > 0) {
                     result = Math.abs(result)
                     list[0] = result
                     displayValue(result)
-                    console.log('jere22222222', list)
-                    console.log(result)
                 } else {
                     result = Math.abs(result)
                     displayValue(result)
-                    console.log('jere')
-                    console.log(result)
                 }
             } else {
                 if (Math.sign(result) == 1) {
                     if (list.length > 0) {
                         result = '-' + result
                         list[0] = result
-                        console.log('hyeeee222222', list)
                         displayValue(result) 
                     } else {
                         result = '-' + result
-                        // list[0] = result
-                        console.log('hyeeee', list)
                         displayValue(result) 
                     }
                 } else if (Math.sign(first) == 1) {
                     first = '-' + first
-                    console.log('lllllaaaaaaa')
                     displayValue(first)
                     console.log(first)
                 }
